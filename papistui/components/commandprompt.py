370e0bb1aee7e67ac30ea5ec4e9befe4778a0e00
supersambo <stephan@schloegl.net>
2025-09-13 20:35:56 +0200

    catch ESC properly

--- papistui/components/commandprompt.py
+++ papistui/components/commandprompt.py
@@ -13,6 +13,7 @@ class CommandPrompt:
         self.cursor_pos = 0
         y, x = self.stdscr.getmaxyx()
         self.win = curses.newwin(1, x, y - 1, 0)
+        self.win.keypad(True)
 
     def display(self):
         self.win.erase()  # Clear only this line
@@ -32,27 +33,44 @@ class CommandPrompt:
         return sum(max(wcwidth(c), 0) for c in chars)
 
     def edit(self, prompt=":", prefill=""):
-        self.input_chars = []
-        self.prompt = prompt
-        self.win.erase()
         self.input_chars = list(prefill)
+        self.prompt = prompt
         self.cursor_pos = len(self.input_chars)
+        self.win.erase()
+        self.win.keypad(True)         # make arrow keys work
+        curses.curs_set(1)            # show cursor while editing
         self.display()
 
         while True:
             ch = self.win.get_wch()
 
             if isinstance(ch, str):
-                if ch == "\n":
+                if ch == "\n":  # Enter
                     break
                 elif ch in ("\x08", "\x7f"):  # Backspace: BS or DEL
                     if self.cursor_pos > 0:
                         del self.input_chars[self.cursor_pos - 1]
                         self.cursor_pos -= 1
+                elif ch == "\x1b":  # ESC pressed
+                    # check if it's really ESC alone (cancel)
+                    self.win.nodelay(True)
+                    try:
+                        nxt = self.win.get_wch()
+                    except curses.error:
+                        nxt = None
+                    self.win.nodelay(False)
+
+                    if nxt is None:
+                        self.input_chars = []
+                        break
+                    else:
+                        # put it back for curses to interpret
+                        curses.unget_wch(nxt)
                 else:
                     if len(self.input_chars) < self.maxlen:
                         self.input_chars.insert(self.cursor_pos, ch)
                         self.cursor_pos += 1
+
             elif ch in (curses.KEY_BACKSPACE, 127):
                 if self.cursor_pos > 0:
                     del self.input_chars[self.cursor_pos - 1]
@@ -70,10 +88,59 @@ class CommandPrompt:
                 self.cursor_pos = 0
             elif ch == curses.KEY_END:
                 self.cursor_pos = len(self.input_chars)
-            elif ch == "\x1b":
+            else:
                 pass
 
             self.display()
 
+        curses.curs_set(0)  # hide cursor again
         self.clear()
         return "".join(self.input_chars)
+
+
+    # def edit(self, prompt=":", prefill=""):
+    #     self.input_chars = []
+    #     self.prompt = prompt
+    #     self.win.erase()
+    #     self.input_chars = list(prefill)
+    #     self.cursor_pos = len(self.input_chars)
+    #     self.display()
+    #
+    #     while True:
+    #         ch = self.win.get_wch()
+    #
+    #         if isinstance(ch, str):
+    #             if ch == "\n":
+    #                 break
+    #             elif ch in ("\x08", "\x7f"):  # Backspace: BS or DEL
+    #                 if self.cursor_pos > 0:
+    #                     del self.input_chars[self.cursor_pos - 1]
+    #                     self.cursor_pos -= 1
+    #             else:
+    #                 if len(self.input_chars) < self.maxlen:
+    #                     self.input_chars.insert(self.cursor_pos, ch)
+    #                     self.cursor_pos += 1
+    #         elif ch in (curses.KEY_BACKSPACE, 127):
+    #             if self.cursor_pos > 0:
+    #                 del self.input_chars[self.cursor_pos - 1]
+    #                 self.cursor_pos -= 1
+    #         elif ch == curses.KEY_LEFT:
+    #             if self.cursor_pos > 0:
+    #                 self.cursor_pos -= 1
+    #         elif ch == curses.KEY_RIGHT:
+    #             if self.cursor_pos < len(self.input_chars):
+    #                 self.cursor_pos += 1
+    #         elif ch == curses.KEY_DC:
+    #             if self.cursor_pos < len(self.input_chars):
+    #                 del self.input_chars[self.cursor_pos]
+    #         elif ch == curses.KEY_HOME:
+    #             self.cursor_pos = 0
+    #         elif ch == curses.KEY_END:
+    #             self.cursor_pos = len(self.input_chars)
+    #         elif ch == "\x1b":
+    #             pass
+    #
+    #         self.display()
+    #
+    #     self.clear()
+    #     return "".join(self.input_chars)
